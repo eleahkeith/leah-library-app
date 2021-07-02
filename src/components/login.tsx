19c3c924a3c94c80585cf73400017de14b28ffd0
Leah Keith <eleahkeith@gmail.com>
2021-07-02 16:52:01 +0100

    feat: add key down functionality for search/login

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { toast } from 'react-toastify';
 import { registerAPI, loginAPI } from './api-calls';
 import Modal from 'react-modal';
@@ -14,11 +14,39 @@ const Login = ({ onLogin }: LoginProps) => {
   const [modalIsOpen, setIsOpen] = useState<boolean>(false);
   const [auth, setAuth] = useState<string>();
 
+  const emailBtn = useRef<HTMLButtonElement>(null);
+  const authBtn = useRef<HTMLButtonElement>(null);
+
   const checkEmail = (email: string | undefined) => {
     if (email) {
       handleSendEmail(email);
     } else {
       toast.error('Please enter an email address');
+    }
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleKeyDown = (e: any) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      if (e.target.id === 'email') {
+        clickEmailBtn();
+      } else if (e.target.id === 'authCode') {
+        clickLoginBtn();
+      }
+    }
+  };
+
+  const clickEmailBtn = () => {
+    if (emailBtn && emailBtn.current) {
+      emailBtn.current.click();
+    }
+  };
+
+  const clickLoginBtn = () => {
+    if (authBtn && authBtn.current) {
+      authBtn.current.click();
+      console.log('auth btn enter');
     }
   };
 
@@ -59,15 +87,18 @@ const Login = ({ onLogin }: LoginProps) => {
             <div className="input-label-group">
               <input
                 className="modal"
+                id="email"
                 type="email"
                 placeholder="e.g. leah@domain.com"
                 onChange={(e) => setEmail(e.target.value)}
+                onKeyDown={handleKeyDown}
               />
               <label htmlFor="email">Email</label>
             </div>
             <button
               type="button"
               className="modal"
+              ref={emailBtn}
               onClick={() => checkEmail(email)}
             >
               Send Email
@@ -97,8 +128,10 @@ const Login = ({ onLogin }: LoginProps) => {
             <div>
               <input
                 className="modal"
+                id="authCode"
                 onChange={(e) => setAuth(e.target.value)}
                 placeholder="Enter code..."
+                onKeyDown={handleKeyDown}
               ></input>
               <label className="modal" htmlFor="authCode">
                 Authorization Code
@@ -109,6 +142,7 @@ const Login = ({ onLogin }: LoginProps) => {
               type="button"
               className="modal"
               onClick={() => checkCode(auth)}
+              ref={authBtn}
             >
               Login
             </button>
